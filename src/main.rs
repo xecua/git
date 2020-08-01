2e16806ab20f6c8a79fcc91ff887679eac792949
xecua <courser4848@gmail.com>
2020-08-02 03:56:45 +0900

    second

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use clap::{clap_app, ArgMatches};
+
 fn main() {
-    println!("Hello, world!");
+    let matches: ArgMatches = clap_app!(app =>
+        (@setting SubcommandRequiredElseHelp)
+        (version: "0.1")
+        (@subcommand commit =>
+            (@arg message: -m * +takes_value "commit message") // エディタ開くのめんどくさいので...
+        )
+    )
+    .get_matches();
+
+    if let Some(opt) = matches.subcommand_matches("commit") {
+        std::process::Command::new("git")
+            .arg("commit")
+            .arg("-m")
+            .arg(opt.value_of("message").unwrap())
+            .spawn();
+    } else {
+        matches.usage();
+    }
 }
